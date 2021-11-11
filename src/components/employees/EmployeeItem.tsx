68b75f6b7b159763eeba73fb8979b1d83d5571f4
Leandro <leandro.urtado25@gmail.com>
2021-11-11 17:22:49 -0300

    Employees page

---
+++ src/components/employees/EmployeeItem.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./EmployeeItem.module.scss";
+
+const EmployeeItem: React.FC<{
+  classNameRow: string;
+  classNameCol: string;
+  employeeData: {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    skype: string;
+    active: boolean;
+    storeId: number;
+    managerId: number;
+  };
+}> = (props) => {
+  return (
+    <div className={props.classNameRow}>
+      <div className={props.classNameCol}>{props.employeeData.id}</div>
+      <div className={props.classNameCol}>{props.employeeData.name}</div>
+      <div className={props.classNameCol}>{props.employeeData.email}</div>
+      <div className={props.classNameCol}>{props.employeeData.phone}</div>
+      <div className={props.classNameCol}>{props.employeeData.skype}</div>
+      <div className={props.classNameCol}>{props.employeeData.active}</div>
+      <div className={props.classNameCol}>{props.employeeData.storeId}</div>
+      <div className={props.classNameCol}>{props.employeeData.managerId}</div>
+    </div>
+  );
+};
+
+export default EmployeeItem;
